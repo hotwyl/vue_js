80fed292bb9f907498d9c3af85e9a33b675a0ad0
William Camargo <william_galdino@econeteditora.com.br>
2022-03-02 07:25:44 -0300

    aula 07

--- curso de vue js 2/aula7/app.js
+++ curso de vue js 2/aula7/app.js
@@ -3,10 +3,10 @@ Vue.createApp({
         return {
             titulo: "Vuejs do jeito ninja!",
             footer: '© 2021 Company, Inc',
-            imagem: 'https://res.cloudinary.com/dm7h7e8xj/image/upload/v1504810464/hello-world-vue_ibatoy.jpg',
-            alt: 'Vuejs is awesome!',
-            title: 'Vuejs is awesome!',
-            classHtml: 'green',
+            btnClassLimpar: 'btn btn-danger btn-sm',
+            btnClassEnviar: 'btn btn-primary btn-lg',
+            btnStyleLimpar: [],
+            btnStyleEnviar: {},
         }
     }
 }).mount('#app');
