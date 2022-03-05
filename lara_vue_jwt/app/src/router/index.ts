2378771d5a6279be9bbfb51bf5f9eeaebbeb57e4
William Camargo <44587916+hotwyl@users.noreply.github.com>
2022-03-05 16:29:08 -0300

    avanço

--- lara_vue_jwt/app/src/router/index.ts
+++ lara_vue_jwt/app/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+import Login from "../views/login.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +10,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
     },
     {
       path: "/about",
