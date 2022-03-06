e46df27116684d7f7fcf54951fc518c08467e8cc
William Camargo <44587916+hotwyl@users.noreply.github.com>
2022-03-06 20:30:47 -0300

    auth

--- lara_vue_jwt/api/app/Http/Controllers/AuthController.php
+++ lara_vue_jwt/api/app/Http/Controllers/AuthController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 
@@ -45,32 +45,32 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    // public function me()
-    // {
-    //     return response()->json(auth()->user());
-    // }
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
 
     /**
      * Log the user out (Invalidate the token).
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    // public function logout()
-    // {
-    //     auth()->logout();
+    public function logout()
+    {
+        auth()->logout();
 
-    //     return response()->json(['message' => 'Successfully logged out']);
-    // }
+        return response()->json(['message' => 'Successfully logged out']);
+    }
 
     /**
      * Refresh a token.
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    // public function refresh()
-    // {
-    //     return $this->respondWithToken(auth()->refresh());
-    // }
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
 
     /**
      * Get the token array structure.
@@ -81,11 +81,7 @@ class AuthController extends Controller
      */
     protected function respondWithToken($token)
     {
-        $user = Auth::user();
-
         return response()->json([
-            'user_name' =>$user->name,
-            'user_email' =>$user->email,
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
--- lara_vue_jwt/api/routes/api.php
+++ lara_vue_jwt/api/routes/api.php
@@ -4,38 +4,16 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::get('/ping', function () {
-    return [
-        'pong' => true
-    ];
-});
-
-Route::post('login', [AuthController::class, 'login']);
-
-// Route::group([
-
-//     'middleware' => 'api',
-//     'prefix' => 'auth'
-
-// ], function ($router) {
-
-//     Route::post('login', [AuthController::class, 'login']);
-//     // Route::post('logout', [AuthController::class, 'logout']);
-//     // Route::post('refresh', [AuthController::class, 'refresh']);
-//     // Route::post('me', [AuthController::class, 'me']);
-// });
-
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::group([
+    // 'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('me', [AuthController::class, 'me'])->name('me');
+});
