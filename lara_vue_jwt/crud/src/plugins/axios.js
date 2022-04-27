f40714071a246e42f04b42f0348f54a497a13d32
William Camargo <william_galdino@econeteditora.com.br>
2022-04-27 17:27:47 -0300

    curso especializa ti

--- lara_vue_jwt/crud/src/plugins/axios.js
+++ lara_vue_jwt/crud/src/plugins/axios.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://dev.3wonline.com/prod/vue/lara_vue_jwt/api/public/api/login'
-axios.defaults.headers.common['Autorization'] = 'bearer ${localStorage.getItem('
-_myapp_token ')}'
+axios.defaults.headers.common['Autorization'] = 'bearer ${localStorage.getItem('_myapp_token ')}'
 
 Vue.useAttrs({
     install(Vue) {
