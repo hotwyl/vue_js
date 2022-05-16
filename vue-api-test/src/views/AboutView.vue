a4eceb791e3c697ab929c3b4ed7f92b2890af9f0
William Galdino <william_galdino@econet.matriz.org>
2022-05-16 10:39:21 -0300

    teste api pokemon

---
+++ vue-api-test/src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+
+  <div v-for="(pokemon, i) in pokemons" :key="i">
+  <p>{{ pokemon.name }}</p></div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import api from "@/services/api";
+
+export default defineComponent({
+name: "about",
+setup(){
+  const pokemons = ref([]);
+  
+  const fetchPokemons = () => api.get("/pokemon?limit=20").then((response) => pokemons.value = response.data.results);
+
+  onMounted(fetchPokemons);
+
+  return {pokemons}
+},
+});
+</script>
